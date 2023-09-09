24a7be7f5cb27bd798e22531ddfc73a828fa61bd
Yo Sasaki <inukichi.i@gmail.com>
2023-09-09 17:55:46 +0900

    first commit, add liquid

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    println!("Hello, world!");
+
+    let template = liquid::ParserBuilder::with_stdlib()
+        .build()
+        .unwrap()
+        .parse("Liquid! {{num | minus: 2}}")
+        .unwrap();
+
+    let globals = liquid::object!({
+        "num": 4f64
+    });
+
+    let output = template.render(&globals).unwrap();
+    println!("{}", output);
+    assert_eq!(output, "Liquid! 2".to_string());
+}
